916148ab1f14d09dc5048027e69d4b72a1efe052
yanuvelir <yanuvelir@gmail.com>
2024-07-09 12:30:46 +0300

    Completed

--- src/Main.java
+++ src/Main.java
@@ -15,15 +15,15 @@ public class Main {
         constraints.anchor = GridBagConstraints.LINE_END;
         constraints.gridx = 0;
         constraints.gridy = 0;
-        panel.add(new JLabel("Name"), constraints);
+        panel.add(new JLabel("Name: "), constraints);
         constraints.gridy++;
-        panel.add(new JLabel("Second name"), constraints);
+        panel.add(new JLabel("Second name: "), constraints);
         constraints.gridy++;
-        panel.add(new JLabel("Email"), constraints);
+        panel.add(new JLabel("Email: "), constraints);
         constraints.gridy++;
-        panel.add(new JLabel("Login"), constraints);
+        panel.add(new JLabel("Login: "), constraints);
         constraints.gridy++;
-        panel.add(new JLabel("Password"), constraints);
+        panel.add(new JLabel("Password: "), constraints);
 
         constraints.anchor  = GridBagConstraints.LINE_START;
         JTextField txtName = new JTextField(8);
@@ -46,15 +46,34 @@ public class Main {
         constraints.gridy ++;
         panel.add(txtLogin, constraints);
 
-        constraints.gridx = 1;
-        constraints.gridy = 5;
-        constraints.anchor = GridBagConstraints.CENTER;
-        panel.add(new JButton("Save"), constraints);
-
         JTextField txtPassword = new JTextField(12);
         constraints.gridx = 1;
         constraints.gridy++;
         panel.add(txtPassword, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 5;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        panel.add(new JLabel("Click the Button to save inputted data. "), constraints);
+
+
+        constraints.gridx = 1;
+        constraints.gridy = 5;
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.gridheight = 2;
+       //constraints.gridwidth = 2;
+        constraints.insets = new Insets(5, 5, 5,5);
+
+        panel.add(new JButton("Save"), constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 6;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        constraints.anchor = GridBagConstraints.LINE_END;
+        //constraints.gridheight = 2;
+
+        panel.add(new JLabel("Do not forget to save the data before exit! - "), constraints);
 
         frame.getContentPane().add(panel);
 
